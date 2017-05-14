f8af1d2f53435b75f859ca749b75f3a0b2fa010e
Robert M Ochshorn <mail@RMOZONE.COM>
2017-05-14 23:53:48 +0200

    process server

--- process_audio.py
+++ process_audio.py
@@ -8,89 +8,94 @@ import os
 import subprocess
 import sys
 
-audio_path = sys.argv[1]
-outdir = sys.argv[2]
+def process(audio_path, outdir, do_transcribe=True):
+    try:
+        os.makedirs(outdir)
+    except OSError:
+        pass
 
-try:
-    os.makedirs(outdir)
-except OSError:
-    pass
+    # Start transcription
+    if do_transcribe:
+        p = subprocess.Popen([
+            'curl',
+            '-o', os.path.join(outdir, 'align.json'),
+            '-X', 'POST',
+            '-F', 'audio=@%s' % (audio_path),
+            'http://localhost:8765/transcriptions?async=false'])
 
-# Start transcription
+    # Generate various effects
 
-p = subprocess.Popen([
-    'curl',
-    '-o', os.path.join(outdir, 'align.json'),
-    '-X', 'POST',
-    '-F', 'audio=@%s' % (audio_path),
-    'http://localhost:8765/transcriptions?async=false'])
+    buf, f0, sp, ap = abstract.analyze(audio_path)
 
-# Generate various effects
+    half_pitch = f0.copy() / 2
+    double_pitch = f0.copy() * 2
 
-buf, f0, sp, ap = abstract.analyze(audio_path)
-
-half_pitch = f0.copy() / 2
-double_pitch = f0.copy() * 2
-
-blur_sp = abstract.windowed_blend(sp, WLEN=50)
+    blur_sp = abstract.windowed_blend(sp, WLEN=50)
 
 
-# Output a bunch of audio
+    # Output a bunch of audio
 
-path = os.path.join(outdir, 'raw.mp3')
-if not os.path.exists(path):
-    nmt.np2sound(
-        (buf*2**15).astype(np.int16),
-        path)
-path = os.path.join(outdir, 'flat.mp3')
-if not os.path.exists(path):
-    flat_pitch = f0.copy()
-    flat_pitch[flat_pitch>0] = f0[f0>0].mean()
-    nmt.np2sound(
-        abstract.gen(flat_pitch, sp, ap),
-        path)
-path = os.path.join(outdir, 'half.mp3')
-if not os.path.exists(path):
-    nmt.np2sound(
-        abstract.gen(half_pitch, sp, ap),
-        path)
-path = os.path.join(outdir, 'double.mp3')
-if not os.path.exists(path):
-    nmt.np2sound(
-        abstract.gen(double_pitch, sp, ap),
-        path)
-path = os.path.join(outdir, 'blur3.mp3')
-if not os.path.exists(path):
-    npblur3 = gaussian_filter(sp, sigma=3)
-    nmt.np2sound(
-        abstract.gen(f0, npblur3, ap),
-        path)
-path = os.path.join(outdir, 'blur30.mp3')
-if not os.path.exists(path):
-    npblur30 = gaussian_filter(sp, sigma=30)
-    nmt.np2sound(
-        abstract.gen(f0, npblur30, ap),
-        path)
-path = os.path.join(outdir, 'fast.mp3')    
-if not os.path.exists(path):
-    nmt.np2sound(
-        abstract.gen(f0, sp, ap, 3),
-        path)
-path = os.path.join(outdir, 'slow.mp3')
-if not os.path.exists(path):
-    nmt.np2sound(
-        abstract.gen(f0, sp, ap, 10),
-        path)
+    path = os.path.join(outdir, 'raw.mp3')
+    if not os.path.exists(path):
+        nmt.np2sound(
+            (buf*2**15).astype(np.int16),
+            path)
+    path = os.path.join(outdir, 'flat.mp3')
+    if not os.path.exists(path):
+        flat_pitch = f0.copy()
+        flat_pitch[flat_pitch>0] = f0[f0>0].mean()
+        nmt.np2sound(
+            abstract.gen(flat_pitch, sp, ap),
+            path)
+    path = os.path.join(outdir, 'half.mp3')
+    if not os.path.exists(path):
+        nmt.np2sound(
+            abstract.gen(half_pitch, sp, ap),
+            path)
+    path = os.path.join(outdir, 'double.mp3')
+    if not os.path.exists(path):
+        nmt.np2sound(
+            abstract.gen(double_pitch, sp, ap),
+            path)
+    path = os.path.join(outdir, 'blur3.mp3')
+    if not os.path.exists(path):
+        npblur3 = gaussian_filter(sp, sigma=3)
+        nmt.np2sound(
+            abstract.gen(f0, npblur3, ap),
+            path)
+    path = os.path.join(outdir, 'blur30.mp3')
+    if not os.path.exists(path):
+        npblur30 = gaussian_filter(sp, sigma=30)
+        nmt.np2sound(
+            abstract.gen(f0, npblur30, ap),
+            path)
+    path = os.path.join(outdir, 'fast.mp3')    
+    if not os.path.exists(path):
+        nmt.np2sound(
+            abstract.gen(f0, sp, ap, 3),
+            path)
+    path = os.path.join(outdir, 'slow.mp3')
+    if not os.path.exists(path):
+        nmt.np2sound(
+            abstract.gen(f0, sp, ap, 10),
+            path)
 
-# Wait for transcription to finish
-p.wait()
+    # Wait for transcription to finish
+    if do_transcribe:
+        p.wait()
 
-trans = json.load(open(os.path.join(outdir, 'align.json')))
+        trans = json.load(open(os.path.join(outdir, 'align.json')))
 
-path = os.path.join(outdir, 'blend.mp3')
-if not os.path.exists(path):
-    wd_blend = abstract.blend_sp_words(sp, trans)
+        path = os.path.join(outdir, 'blend.mp3')
+        if not os.path.exists(path):
+            wd_blend = abstract.blend_sp_words(sp, trans)
 
-    nmt.np2sound(
-        abstract.gen(f0, wd_blend, ap),
-        path)
+            nmt.np2sound(
+                abstract.gen(f0, wd_blend, ap),
+                path)
+
+if __name__=='__main__':
+    audio_path = sys.argv[1]
+    outdir = sys.argv[2]
+
+    process(audio_path, outdir)
---
+++ process_serve.py
@@ -0,0 +1,84 @@
+import json
+import os
+import uuid
+import tempfile
+
+from twisted.web.static import File
+from twisted.web.resource import Resource
+from twisted.web.server import Site, NOT_DONE_YET
+from twisted.internet import reactor, threads
+
+import process_audio
+import urllib
+import requests
+
+# Process over HTTP
+
+POST_URL = 'https://voxanon.knowtheory.net/messages/notify'
+PROCESS_ROOT = 'http://process.text.audio/'
+
+filter_opts = ['raw', 'half', 'double', 'blend', 'flat', 'blur3', 'blur30', 'fast', 'slow']
+
+
+class Process(Resource):
+    def __init__(self, rootdir):
+        self.rootdir = rootdir
+        Resource.__init__(self)
+
+    def next_id(self):
+        uid = None
+        while uid is None or os.path.exists(os.path.join(self.rootdir, uid)):
+            uid = uuid.uuid4().get_hex()[:16]
+        return uid
+
+    def render_POST(self, req):
+        cmd = json.load(req.content)
+
+        audio_url = cmd['fb_audio_url']
+
+        reactor.callInThread(self.do_process, cmd)
+
+        return 'thanks!'
+
+    def do_process(self, cmd):
+        uid = self.next_id()
+        
+        # Download audio
+        a_url = cmd['fb_audio_url']
+        with tempfile.NamedTemporaryFile(suffix=a_url.split('.')[-1]) as fp:
+
+            print 'downloading', a_url
+            urllib.urlretrieve(a_url, filename=fp.name)
+
+            print 'processing'
+            process_audio.process(fp.name, os.path.join(self.rootdir, uid))
+
+        cmd['process_uid'] = uid
+        cmd['filters'] = {}
+        for name in filter_opts:
+            cmd['filters'][name] = PROCESS_ROOT + uid + '/%s.mp3' % (name)
+
+        # Done! Notify
+        print 'done!', uid, 'posting', cmd
+        requests.post(POST_URL, json=cmd)
+        print 'ok'
+
+if __name__=='__main__':
+    MEDIA_ROOT = 'media'
+    port = 7890
+    interface = '0.0.0.0'
+
+    try:
+        os.makedirs(MEDIA_ROOT)
+    except OSError:
+        pass
+
+    f = File(MEDIA_ROOT)
+
+    f.putChild('process', Process(MEDIA_ROOT))
+
+    s = Site(f)
+    reactor.listenTCP(port, s, interface=interface)
+    print 'http://%s:%d' % (interface, port)
+    reactor.run()
+    
